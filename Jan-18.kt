b18289d76334b920f5d727c73c27181376355f37
Ice-Citron <shng2025@gmail.com>
2026-01-19 01:36:14 +0000

    sync 1

---
+++ Jan-18.kt
@@ -0,0 +1,107 @@
+
+
+
+/*
+       !
+       !
+       ^
+      / \
+     /___\
+    |=   =|
+    |     |
+    |  S  |
+    |  I  |
+    |  E  |
+    |  N  |
+    |  A  |
+    |  R  |
+    |     |
+    |     |
+   /|##!##|\
+  / |##!##| \
+ /  |##!##|  \
+|  / ^ | ^ \  |
+| /  ( | )  \ |
+|/   ( | )   \|
+    ((   ))
+   ((  :  ))
+   ((  :  ))
+    ((   ))
+     (( ))
+      ( )
+       .
+       .
+       .
+* */
+
+
+/*
+    Here is a walkthrough of the "Resizing Array-Based List" slides you uploaded
+    . This is a foundational Data Structures topic.
+
+    If you have used `std::vector` in C++ or `ArrayList` in Java, you have used
+    exactly what we are about to build.
+
+    ------
+
+    1. The Concept: "Smart" ARRAYS
+
+    SLIDE REFERENCE:
+    A standard array (like `int arr[5]` in C++ or `Array(5)` in Kotlin) has a
+    FIXED CAPACITY.
+
+    - PROBLEM: If you pick a size too small, you run out of space. If you pick
+      one too big, you waste memory.
+    - SOLUTION: A RESIZING LIST. It starts small, and when it gets full, it
+      automatically grows ()
+
+
+ */
+
+
+
+
+
+/*
+                                                _
+                  ___                          (_)
+                _/XXX\
+ _             /XXXXXX\_                                    __
+ X\__    __   /X XXXX XX\                          _       /XX\__      ___
+     \__/  \_/__       \ \                       _/X\__   /XX XXX\____/XXX\
+   \  ___   \/  \_      \ \               __   _/      \_/  _/  -   __  -  \__/
+  ___/   \__/   \ \__     \\__           /  \_//  _ _ \  \     __  /  \____//
+ /  __    \  /     \ \_   _//_\___     _/    //           \___/  \/     __/
+ __/_______\________\__\_/________\_ _/_____/_____________/_______\____/_______
+                                   /|\
+                                  / | \
+                                 /  |  \
+                                /   |   \
+                               /    |    \
+                              /     |     \
+                             /      |      \
+                            /       |       \
+                           /        |        \
+                          /         |         \
+* */
+
+
+
+
+/*nunununununununununununununununununununununununununununununununununununununununununununununununununununununununununununu*/
+
+
+/*
+* conntinue to new 4        <-- then we will switch to other modules!
+*                               https://gemini.google.com/app/bd659ecdaa303cef
+*                               https://gemini.google.com/app/40c2e1110b00cd2a
+* finish PPT-5 kotlin
+*
+            * DO INFORMATION 2-4 SLIDE EXERCISES <-- ASKS GEMINI 3 TO WRITE EXERCISE AND
+            * TEST CASES FOR US
+*
+* ask more detail about how does the technique of casting <Any?> then back to `as <T>` work in more detail
+* ... like I can kind of see how it works but not fully at the same time.
+* */
+
+
